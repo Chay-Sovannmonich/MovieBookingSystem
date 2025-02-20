1c243af878cee88415aedbab82cb7b86edfd8c3b
Saul <Venghourt.chhoeun@student.cadt.edu.kh>
2025-02-20 21:07:58 +0700

    Renamed file from old_filename to new_filename

---
+++ Movie_hourt.java
@@ -0,0 +1,29 @@
+
+public class Movie_hourt implements Authenticator{
+    public static void main(String[] args) {
+
+        // Admin admin1 = new Admin( "admin1@gmail.com", "01234567", "Admin1", "12345");
+        // Admin admin2 = new Admin( "admin2@gmail.com", "01234567", "Admin2", "23456" );
+
+        // System.out.println("admin1 id: " + admin1.getId("12345"));
+        // System.out.println("admin2 id: " + admin2.getId("23456"));
+
+        // Review r1 = new Review("user1", "Avenger", 5.0 , "Good movie", "2/14/2025", "1");
+
+        // System.out.println("reviewID: " + r1.getReviewID());
+
+    
+    }
+
+    @Override
+    public void login(String ID, String password) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void register(String email, String password) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
